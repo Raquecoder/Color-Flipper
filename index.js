43199ecd7982b108402487d5c6f35de04d413776
Raquel Campos <112841246+Raquecoder@users.noreply.github.com>
2023-04-16 17:54:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const colors = ["green","yellow","red","pink","purple","orange","blue"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click",function(){
+    //get random number between 0-6
+    const randomNumber= getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length) ;
+}
